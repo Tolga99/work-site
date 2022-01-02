4433335da741d2e2b76a6c88e3d54da05253772e
Tolga Ovali <tolga.ovali@student.hepl.be>
2022-01-02 17:10:13 +0100

    Réaregemment fenêtre worksite Test pdf

--- src/app/tb-settings/tb-settings.page.ts
+++ src/app/tb-settings/tb-settings.page.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { jsPDF} from 'jspdf'
 @Component({
   selector: 'app-tb-settings',
   templateUrl: 'tb-settings.page.html',
   styleUrls: ['tb-settings.page.scss']
 })
-export class TabSettingsPage {
+export class TabSettingsPage implements OnInit {
 
   constructor(private router:Router) {}
+  ngOnInit(): void {
+    const doc = new jsPDF();
+
+    doc.text("Hello world!", 10, 10);
+    doc.save("a4.pdf");  }
   MyProfile()
   {
     console.log('show profile');
